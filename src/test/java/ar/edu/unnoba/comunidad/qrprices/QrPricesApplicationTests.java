2688681cd11d50193250057afe6a198ef8c54e10
ValentinoBadano <valentinobadano@gmail.com>
2023-10-13 15:07:27 -0300

    let's go

---
+++ src/test/java/ar/edu/unnoba/comunidad/qrprices/QrPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.comunidad.qrprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
